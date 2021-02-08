2cc9646e477e4f72c5b95be2618b10724609c800
Jonathan Weiss <weissjon@gmail.com>
2021-02-08 08:28:45 +0200

    change AJAX endpoints to be relative

--- client/src/AJAX/ajax-endpoints.js
+++ client/src/AJAX/ajax-endpoints.js
@@ -1,7 +1,7 @@
-// const USER_SERVER_URL = "/api/";
 const USER_SERVER_URL = "http://weiss-english-server.herokuapp.com/api/";
-const DOMESTIC_SERVER_URL = "https://weiss-class.herokuapp.com/api/";
+// const DOMESTIC_SERVER_URL = "https://weiss-class.herokuapp.com/api/";
 // const DOMESTIC_SERVER_URL = "http://localhost:3000/api/";
+const DOMESTIC_SERVER_URL = "/api/";
 // const USER_SERVER_URL = "http://localhost:5001/api/";
 
 export { USER_SERVER_URL };
